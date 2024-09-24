b14a4f18b3b3613ab149f2b14c6d9be709e08925
midnightcrow00 <orsolyaalbert1@gmail.com>
2024-09-24 10:40:24 +0200

    esemenyKezelo

--- model/Model.js
+++ model/Model.js
@@ -6,5 +6,10 @@ class Modell {
         this.#lista = [false, false, false, false, false,false,false,false,false];
         this.#db = 0;
     }
-
+    let txt="";
+    for (let i = 0; i< 9; i++) {
+        txt+= `<div class="lampa" id="${i}"><p></p></div>`
+    }
+    txt += "";
+    this.#szuloElem.append(txt)
  }
--- view/ElemView.js
+++ view/ElemView.js
@@ -1,26 +1,19 @@
 class ElemView {
     #index;
-    constructor(szuloElem) {
+    constructor(szuloElem,index) {
+        this.#index = index;
         this.szuloElem=szuloElem;
         this.kattinthato = true;
         this.#htmlOsszerak();
         this.elem = $(".elem:last-child");
     this.pElem = this.elem.children("p");
     this.elem.on("click", () => {
-      if (this.kattinthato) {
-        this.#sajatEsemenykezelo("kivalaszt");
-        this.kattinthato = false;
-      }
+     
     });
     }
 
     #htmlOsszerak(){
-        let txt="";
-        for (let i = 0; i< 9; i++) {
-            txt+= `<div class="lampa" id="${i}"><p></p></div>`
-        }
-        txt += "";
-        this.#szuloElem.append(txt)
+       
     }
     
  }
